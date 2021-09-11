2c1fdcc7f84fac7b85f6ccadb38b779ce6e4d2e2
Jeffrey Smithwick <71730271+JSMTHWCK@users.noreply.github.com>
2021-09-11 20:42:46 +0000

    transpose done

--- src/matrix.py
+++ src/matrix.py
@@ -8,6 +8,16 @@ class Matrix:
 	def print(self):
 		for item in self.vector:
 			print(item)
+	
+	def transpose(self):
+		for a in range(0,2):
+			new_array = []
+			for item in self.vector:
+				new_array.append(item[a])
+			print(new_array)	
+
+
 
 a = Matrix([[1,2],[3,4]])
 a.print()
+a.transpose()

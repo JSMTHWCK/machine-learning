f836d648e2d8be5ad48b59e40d49d515c42fd40a
Jeffrey Smithwick <71730271+JSMTHWCK@users.noreply.github.com>
2021-09-14 22:35:03 +0000

    did proper return

--- src/matrix.py
+++ src/matrix.py
@@ -1,23 +1,55 @@
+
+
 class Matrix:
-	def __init__(self,vector):
+	def __init__(self,vector): # elements
 		self.vector = vector
-		self.row = len(vector)
-		self.column = len(vector[0])
-
-
+		self.row = len(self.vector[0])
+		self.column = len(self.vector)
+        
 	def print(self):
 		for item in self.vector:
 			print(item)
-	
+        
 	def transpose(self):
+		final_array = []
 		for a in range(0,2):
 			new_array = []
 			for item in self.vector:
 				new_array.append(item[a])
-			print(new_array)	
+			final_array.append(new_array)
+		return Matrix(final_array)
+
+	def add(self, vector2):
+		final_array = []
+		for item in range(0,2):
+			new_array = []
+			for a in range(0,2):
+				new_array.append(self.vector[item][a] + vector2[item][a])
+			final_array.append(new_array)
+		return Matrix(final_array)
+
+	def scalar(self,scalar):
+		final_array = []
+		for item in self.vector:
+			new_array = []
+			for a in range(0,2):
+				new_array.append(scalar *item[a])
+			final_array.append(new_array)
+		return Matrix(final_array)
+'''
+	def dot_product(self,vector1,vector2):
+		return vector1[0] * vector2[0] + vector1[1] *vector2[1]
+'''
 
 
+				
 
+
+        
+        
 a = Matrix([[1,2],[3,4]])
-a.print()
-a.transpose()
+#a.print()
+
+a.transpose().print()
+a.scalar(3).print()
+a.add([[1,2],[3,4]]).print()
---
+++ thought.md
@@ -0,0 +1,5 @@
+Todo: <br>
+~~Fix transpose~~ <br>
+~~Fix add~~ </br>
+Fix scalar </br>
+Start multiply </br>

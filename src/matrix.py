b76715399b5549124800cdbfa4df40a9cd59af87
Jeffrey Smithwick <71730271+JSMTHWCK@users.noreply.github.com>
2021-09-23 22:17:12 +0000

    after 3 years... it's done (matrix mult generalized)

--- src/matrix.py
+++ src/matrix.py
@@ -2,71 +2,104 @@ class Matrix:
 
 	def __init__(self,elements): # elements
 		self.elements = elements
-		self.column = len(self.elements[0])
-		self.row = len(self.elements)
+		self.numcols = len(self.elements[0])
+		self.numrows = len(self.elements)
         
 	def print(self):
-
-		for item in self.elements:
-			print(item)
+		
+		for row in self.elements:
+			print(row)
         
 	def transpose(self):
 
 		final_array = []
-		for a in range(0,2):
+		for a in range(0,len(self.numrows)):
 			new_array = []
-			for item in self.elements:
-				new_array.append(item[a])
+			for row in self.elements:
+				new_array.append(row[a])
 			final_array.append(new_array)
 		return Matrix(final_array)
 
 	def add(self, elements2):
-		if self.row != len(elements2) or self.column !=len(elements2[0]):
+		if self.numrows != len(elements2) or self.numcols !=len(elements2[0]):
 			print("matricies are of unequal dimensions")
-			return
+			return 
 		final_array = []
-		for item in range(0,self.row):
+		for i in range(0,self.numrows):
 			new_array = []
-			for a in range(0,self.column):
-				new_array.append(self.elements[item][a] + elements2[item][a])
+			for a in range(0,self.numcols):
+				new_array.append(self.elements[i][a] + elements2[i][a])
 			final_array.append(new_array)
 		return Matrix(final_array)
 
 	def scalar(self,scalar):
 
 		final_array = []
-		for item in self.elements:
+		for row in self.elements:
 			new_array = []
-			for a in range(0,self.column):
-				new_array.append(scalar * item[a])
+			for a in range(0,self.numcols):
+				new_array.append(scalar * row[a])
 			final_array.append(new_array)
 		return Matrix(final_array)
 
 	def dot_product(self,vector1,vector2):
+		x = 0
+		for i in range(0,len(vector1)):
+			#print(len(vector1))
+			#print(len(vector2))
+			#print(vector1)
+			#print(vector2)
+			x += vector1[i] * vector2[i]
+		return x
 
-		return vector1[0] * vector2[0] + vector1[1] * vector2[1]
-
-	def col_to_row(self,matrix,position):
+	def col_to_row(self,position):
 
 		row = []
-		for item in range(0,2):
-			row.append(matrix[item][position])
+		for i in range(0,self.numrows):
+			row.append(self.elements[i][position])
 		return row
 	
 	def matrix_multiplication(self,matrix2):
-
+		if self.numcols != matrix2.numrows:
+			print("matricies are incompatable")
+			#print(self.numcols)
+			#print(matrix2.numrows)
+			return
+		else: 
+			print("it works")
 		final_array = []
-		for row in range(0,2):
+		for rowindex in range(0,self.numrows):
 			new_array = []
-			for column in range(0,2):
-				dot_prod = self.dot_product(self.elements[row],self.col_to_row(matrix2,column))
+			for colindex in range(0,matrix2.numcols):
+				row = self.elements[rowindex]
+				col = matrix2.col_to_row(colindex)
+				print("entry ", rowindex,colindex)
+				print("row " + str(row))
+				print("col " + str(col))
+				dot_prod = self.dot_product(row,col)
+				print("dot " + str(dot_prod))
+				print('')
 				new_array.append(dot_prod)
+				#print(dot_prod)
+				#print('')
+				#print(new_array)
 			final_array.append(new_array)
 		return Matrix(final_array)
 
+		
+
+a = Matrix([[3,10],[3,10]])
+b = Matrix([[3,10],[0,2],[1,2]])
+c = Matrix([[2,3],[1,2]])
 
 
-a = Matrix([[3,10,24],[3,10,24]])
 #a.print()
-a.add([[3,10,24],[24,13,5]]).print()
-a.scalar(3).print()
+#if a.add([[3,10,24,4],[24,13,5,4]]) != None:
+#	a.add([[3,10,24,4],[24,13,5,4]]).print()
+
+#a.scalar(3).print()
+#a.col_to_row([[1,2,3],[3,5,9]],2).print()
+#b.print()
+#print(a.dot_product([1,2,3,4,5],[1,2,3,4,5]))
+b.matrix_multiplication(c).print()
+#,[2,3],[4,5],[7,8]
--- thought.md
+++ thought.md
@@ -1,14 +1,11 @@
-Todo: <br>
-~~Fix transpose~~ <br>
-~~Fix add~~ </br>
-~~Fix scalar~~ </br>
-~~vector --> elements~~ </br>
-Start multiply </br>
-
-
-**multiply** </br>
-~~need helper function to make column to row~~ </br>
->can't transpose original matrix because of indexes</br>
-
-get dot product working </br>
-> be able to call it inside helper function between matrix[pos] and transposed
+Todo: </br>
++ generalizing matrix class </br>
+  + ~~add~~
+  + ~~scalar~~
+  + ~~dot~~
+  + ~~transpose~~
+  + multiplication
++ recursive det
+  + get standard det working
+  + get sub matrix helper function working
+  + get recursion working

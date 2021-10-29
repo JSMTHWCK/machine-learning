5f5a5aa32d486d91b2619fcfc5bc51aa0870b11e
JSMTHWCK <jeffrey.smithwick@gmail.com>
2021-10-29 19:58:08 +0000

    breaks at if item not visited (still progress)

--- src/graph.py
+++ src/graph.py
@@ -16,9 +16,23 @@ class Graph:
 			if self.children[i] == value:
 				empty_array.append(self.parents[i])
 		return empty_array
+	def bfs(self):
+		queue = [self.parents[0]]
+		visited = {self.parents[0]:True}
+		order = [self.parents[0]]
+		while len(queue) != 0:
+			for item in self.get_children(queue[0]):
+				queue.append(item)			
+			queue.pop(0)
+			for item in queue:
+				if item not in visited:
+					queue[item] = True
+					order.append(item)
+		return order
+
 
 graph = Graph([[2,8],[9,3],[1,5],[9,6],[9,6],[6,9],[4,5],[10,6],[3,10],[8,7],[8,5],[6,2],[8,10],[6,1],[6,4]])
 
 print(graph.get_parents(6))
-
+print(graph.bfs())
 

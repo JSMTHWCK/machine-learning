094b63bb1cf37bc2d67d00508a7bac581491f639
JSMTHWCK <jeffrey.smithwick@gmail.com>
2021-11-28 04:20:25 +0000

    reverted analysis

--- src/analysis.py
+++ src/analysis.py
@@ -1,60 +1,29 @@
 from matrix import Matrix
 
-class linearSandwich():
-	def fit(self, data,interaction_terms=False):
+class LinearRegressor():
+
+	def fit(self, data):
 		n = len(data[0])
 		rows = [[point[n-1]] for point in data]
 
 		y_matrix = Matrix(rows)
 
-
-
 		m_b = [point[0:n-1] for point in data]
 		for item in range(0,len(m_b)):
-			if interaction_terms == True:
-				for x in range(1,len(data[item])):
-					for y in range(2,len(data[item])):
-						if y > x:
-							m_b[item].append(data[item][y] * data[item][x])				
 			m_b[item].append(1)
 		data_matrix = Matrix(m_b)	
-		
 		transposed_data = data_matrix.transpose()
-
 		left_side = transposed_data.matrix_multiplication(y_matrix)
-
 		right_side = transposed_data.matrix_multiplication(data_matrix)
 		right_side = right_side.inverse()
 		right_side = right_side.matrix_multiplication(left_side)
-
-		self.coefficients = []
 		for i in range(right_side.numrows):
 			self.coefficients.append(right_side.elements[i][0])
 
 	def predict(self,values):
-		total = 0 
-		index = 0
-		for a in range(1,len(values)):
-			for b in range(1,len(values)):
-				if a==b:
-					total += self.coefficients[a] * values[a]
-				elif b>a:
-					total += self.coefficients[index] * values[a]*values[b]
-				index += 1
-		total += self.coefficients[-1]
-		
+		total = 0
+		for i in range(0,len(values)):
+			total += self.coefficients[i] * values[i]
+		total +=  self.coefficients[-1]
 		return total
 
-
-
-a = linearSandwich()
-data_0 = [[0, 0, 1], [1, 0, 2], [2, 0, 4], [4, 0, 8], [6, 0, 9], [0, 2, 2], [0, 4, 5], [0, 6, 7], [0, 8, 6]]
-data = [[0,0,1],[1,0,2],[2,0,4],[4,0,8],[6,0,9],[0,2,2],[0,4,5],[0,6,7],[0,8,6],[2,2,1],[3,4,1]]
-a.fit(data,True)
-print(a.coefficients)
-print(a.predict([5,0]))
-print(a.predict([5,5]))
-
-sandwich = open("./tests/hotdog_test.txt","w")
-sandwich.write("input of [5,0] gives {} \n".format(a.predict([5,0])))
-sandwich.write("input of [5,5] gives {} \n".format(a.predict([5,5])))

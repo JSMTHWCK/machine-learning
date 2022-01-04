bfbe635244daf5af9a974c913dc847b98ceb0b8e
JSMTHWCK <jeffrey.smithwick@gmail.com>
2022-01-03 21:25:31 -0800

    logistic coefficients work

--- src/knn.py
+++ src/knn.py
@@ -1,20 +1,3 @@
 import math
 
-def calc_minimum(numbers):
-    a = numbers[0]
-    for n in numbers:
-        if n < a:
-            a = n
-    return a
-
-class KnnClassifier:
-    def __init__(self,k):
-        self.k = k 
-    def fit(self,data,classification):
-        self.data = data
-        self.classification = classification
-    def compute_distance(self,observation):
-        distances = []
-        for i in self.data:
-            distances.append(0)
-        
+    

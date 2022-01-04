787693f6f88549e26b30463394b1675e895fcb35
JSMTHWCK <jeffrey.smithwick@gmail.com>
2022-01-03 21:50:51 -0800

    multi var grad descent works w/ test cases

--- src/gradient_descent.py
+++ src/gradient_descent.py
@@ -4,5 +4,11 @@ def minimize(prime,x,step,iterations):
         x = x - step * prime(x)
     return x
 
+def multivariable_minimize(functions,initial_points,step = 0.001,iteration=1000):
+    for i in range(0,iteration):
+        for i in range(0,len(functions)):
+            prime = functions[i](initial_points[i])
+            initial_points[i] -= step * prime 
+    return initial_points
 def xsquared(x):
     return 2 * x
--- tests/gradient_tests.py
+++ tests/gradient_tests.py
@@ -1,14 +1,31 @@
 import sys
 import math
-sys.path.append('src')
 
-from gradient_descent import minimize
+sys.path.append('src')
+from matrix import roundarray
+
+from gradient_descent import minimize, multivariable_minimize
 
 def xsquared(x):
     return 2 * x
 
 def x_test(x):
     return 2 * x + 9 * (x ** 2) + 4 * (x ** 3) + 3
+
+def fpx(x):
+    return 2 * x
+def fpy(y):
+    return 2 * (y-2)
+def fpz(z):
+    return 3 * (z-3)
+
+def fpx2(x):
+    return 2 * (x-1)
+def fpy2(y):
+    return 2 * (y-1)
+def fpz2(z):
+    return 3 * (z-1)
+
 
 if round(minimize(xsquared,-12,0.91,10000),3) != 0:
     print("minimize failed on xsquared")
@@ -19,4 +36,16 @@ if round(minimize(x_test, 10, 0.001, 100000),2) != -2.18:
     print('minimize failed on x_test')
     print('wanted -2.18')
     print('got ',round(minimize(x_test,10,0.001,100000),2))
+print('.')
+
+if roundarray(multivariable_minimize([fpx,fpy,fpz],[1,1,1]) , 3) != [0.135,1.865,2.901]:
+    print('multi descent failed')
+    print('wanted [0.13,1.86,2.90]')
+    print('got ',roundarray(multivariable_minimize([fpx,fpy,fpz],[1,1,1]),3))
+
+
+if roundarray(multivariable_minimize([fpx2,fpy2,fpz2],[1,1,1]) , 3) != [1,1,1]:
+    print('multi descent failed')
+    print('wanted [1,1,1]')
+    print('got ',roundarray(multivariable_minimize([fpx2,fpy2,fpz2],[1,1,1]),3))
 print('.')

e973fa473badefb231da0ea3bc80d844175324ac
JSMTHWCK <jeffrey.smithwick@gmail.com>
2022-01-07 15:18:03 -0800

    rss works

---
+++ tests/rstest.py
@@ -0,0 +1,19 @@
+import sys
+sys.path.append('src')
+
+from rss import *
+from matrix import *
+
+
+def b_one_grad(item):
+    return ((10 * item[0]) + (6 * item[1]) - 18)
+
+def a_one_grad(item):
+    return ((26 * item[0]) + (10 * item[1]) - 42)
+
+rss_one = fit([1,0], [a_one_grad, b_one_grad],0.001,1000000)
+
+if roundarray(rss_one,4) != [1.2857,0.8571]:
+    print('rss failed')
+    print('wanted [1.2857,0.8571]')
+    print('got ',rss_one)

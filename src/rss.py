0aa458e9d9c3b426d8cd5a0f70fa99c7c4635598
JSMTHWCK <jeffrey.smithwick@gmail.com>
2022-01-12 15:42:55 -0800

    testing something

--- src/rss.py
+++ src/rss.py
@@ -4,6 +4,8 @@ import gradient_descent
 def fit(data_points, functions,step = 0.001,iteration = 1000):
     data_points_n = data_points
     for i in range(iteration):
+        if i%1000 == 0:
+            print(data_points)
         data_points = list(data_points_n)
         for item in functions:
             data_points_n[functions.index(item)] = data_points[functions.index(item)] - step * item(data_points)
